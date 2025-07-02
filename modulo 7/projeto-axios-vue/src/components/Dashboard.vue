0b1c448c8cb801f68c0c9460f9c99e2038d43ab2
Vinis-vp <viniciusviertel955@gmail.com>
2025-07-02 15:05:32 -0300

    Feito o dashboard

---
+++ modulo 7/projeto-axios-vue/src/components/Dashboard.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container mt-5">
+    <h2>Área Protegida</h2>
+    <p>Somente usuários autenticados veem isso.</p>
+    <button class="btn btn-danger" @click="logoutUser">Sair</button>
+  </div>
+</template>
+
+<script setup>
+import { logout } from '@/services/auth'
+
+const logoutUser = () => {
+  logout()
+  window.location.href = '/'
+}
+</script>
